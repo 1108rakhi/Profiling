4f8e6844d6223ea760c35ee198f754d4d39438fb
Rakhi <1108rakhi@gmail.com>
2025-09-15 12:41:17 +0530

    Created profiling report which saves the report in a csv format

--- file.py
+++ file.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import argparse
 
-def profiling(file_path):
+def profiling(file_path, save_csv = False):
+    # version 1 
     df = pd.read_csv(file_path)
     total_rows = len(df)
     total_cols = df.shape[1]
@@ -11,10 +12,38 @@ def profiling(file_path):
     duplicate_percentage = round((duplicate_count / total_rows) * 100, 2)
     print('Total rows are:\n', total_rows)
     print('Total columns are:\n', total_cols)
-    print('Count of null values:\n', null_counts)
-    print('Percentage of null values:\n', null_percentage)
-    print('Number of duplicates:\n', duplicate_count)
-    print('Perccentage of duplicates:\n', duplicate_percentage)
+    for col in df.columns:
+        col_dup_count = df[col].duplicated().sum()   
+        col_dup_percent = round((col_dup_count / total_rows) * 100, 2)
+
+        print(f"Column: {col}")
+        print(f"  Null Count: {null_counts[col]}")
+        print(f"  Null %: {null_percentage[col]}%")
+        print(f"  Duplicate Count: {col_dup_count}")
+        print(f"  Duplicate %: {col_dup_percent}%")
+
+# version 2 --> csv based output
+    if save_csv:
+        metrics = []
+        metric_types = ["Total Rows","Total Columns","Row-wise Duplicate Count","Row-wise Duplicate %"]
+        values = [total_rows, total_cols, duplicate_count, duplicate_percentage]
+        metrics.extend({"Metric Type": m, "Value": v} for m, v in zip(metric_types, values))
+        for col in df.columns:
+                col_dup_count = df[col].duplicated().sum()
+                col_dup_percent = round((col_dup_count / total_rows) * 100, 2)
+                metric_types = [
+                    f"Null Count [{col}]",
+                    f"Null % [{col}]",
+                    f"Duplicate Count [{col}]",
+                    f"Duplicate % [{col}]"
+                ]
+                values = [null_counts[col], null_percentage[col], col_dup_count, col_dup_percent]
+                metrics.extend({"Metric Type": m, "Value": v} for m, v in zip(metric_types, values))
+
+        report = pd.DataFrame(metrics)
+        output_file = file_path.replace(".csv", "_profiling_report.csv")
+        report.to_csv(output_file, index=False)
+        print('csv saved')
 
 
 if __name__ == "__main__":
@@ -25,8 +54,9 @@ if __name__ == "__main__":
     default=r"C:\Users\Rightdatauser\Documents\CLI_Based_Profiling\profiling_demo.csv",
     help="Path to the CSV file"
 )
+    parser.add_argument("--csv", action="store_true", help="Also save metrics to CSV")
 
     args = parser.parse_args()
 
-    profiling(args.file)
+    profiling(args.file, save_csv = args.csv)
       
